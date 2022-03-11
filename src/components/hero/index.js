1c68be7161586ff3a489105b0b49fea82625c962
CB <ybloc1@outlook.com>
2022-03-11 14:03:19 -0500

    removed uncessary files

---
+++ src/components/hero/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Heatmap from '../../assets/heatmap.png';
+//  prettier-ignore
+
+const Hero = () => {
+  const params = useParams();
+  return (
+    <>
+      <h1>No reactions to your reddit posts?</h1>
+      <h3>
+        Great timing, great results! Find the best time to post on your
+        subreddit.
+      </h3>
+      <Link to={`/search/${params.term}`}>
+        <button type="button">SHOW ME THE BEST TIME</button>
+      </Link>
+      <input type="text" placeholder="Search..." defaultValue={params.term} />
+      <image src={Heatmap} alt="heatmap" />
+    </>
+  );
+};
+
+export default Hero;
